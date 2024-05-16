1f2ca67174b774c9f69f9a0a8202a00e8854d4f8
Mikhail Tabakaev <47657224+Mihakgma@users.noreply.github.com>
2024-05-16 14:00:29 +0700

    7_3 lesson has been completed

---
+++ 1_JavaSyntax/7_lvl/code_examples/_7_3_final_attribute/src/Cat.java
@@ -0,0 +1,8 @@
+public class Cat {
+    public static final int maxAge = 25;
+    public final int maxWeight;
+
+    public Cat (int weight) {
+        this.maxWeight = weight;
+    }
+}
---
+++ 1_JavaSyntax/7_lvl/code_examples/_7_3_final_attribute/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Cat cat1 = new Cat(10);
+        System.out.println(Cat.maxAge);
+//        cat1.maxWeight = 11; Cannot assign a value to final variable 'maxWeight'
+//        System.out.println("Hello world!");
+    }
+}

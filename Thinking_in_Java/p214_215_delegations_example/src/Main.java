d8fb88cf97a7ac77a72a7b1742e8349ad50495a1
Mikhail Tabakaev <47657224+Mihakgma@users.noreply.github.com>
2024-06-26 14:19:10 +0700

    thinking in java added with new examples

---
+++ Thinking_in_Java/p214_215_delegations_example/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        SpaceShipDelegation protector = new SpaceShipDelegation("NSEA Protector");
+        protector.forward(100);
+        System.out.println("Hello world!");
+    }
+}

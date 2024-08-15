18700e8ecc74cfd45d3497763bda76b16a3dbbb4
Mikhail Tabakaev <47657224+Mihakgma@users.noreply.github.com>
2024-08-15 13:48:48 +0700

    thinking in java initialized p252 polymorph example

---
+++ Thinking_in_Java/p_252_253_polymorph_dispose_animals/src/Characteristic.java
@@ -0,0 +1,12 @@
+public class Characteristic {
+    private String s;
+
+    Characteristic(String s) {
+        this.s = s;
+        Main.print("Creating Characteristic " + s);
+    }
+
+    protected void dispose() {
+        Main.print("Finalizing Characteristic " + s);
+    }
+}
